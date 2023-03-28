36e8baf5931dd6e9c3efe3c9faebab1f19c9959e
Guru Mahan <iosdeve2022@gmail.com>
2023-03-28 17:37:46 +0530

    faceMatch auth in swift

--- FaceAuthXIB_POC/SceneDelegate.swift
+++ FaceAuthXIB_POC/SceneDelegate.swift
@@ -1,22 +1,32 @@
 //
 //  SceneDelegate.swift
-//  FaceAuthXIB_POC
+//  Catalog
 //
-//  Created by Guru Mahan on 28/03/23.
+//  Created by Pavel Kondrashkov on 5/18/21.
 //
 
 import UIKit
 
+@available(iOS 13, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+
+        let container = UINavigationController()
+        container.navigationBar.prefersLargeTitles = true
+
+        container.viewControllers = [MatchFacesRequestViewController()]
+        window.rootViewController = container
+
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,7 +56,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
